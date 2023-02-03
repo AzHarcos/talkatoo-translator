66fe82f24e73ee0b9e47e7151ca67e95573ce159
Biakko <58895947+biakkobot@users.noreply.github.com>
2023-02-02 22:37:42 -0500

    Initial/mostly successful collected moon checker

--- Talkatoo.py
+++ Talkatoo.py
@@ -119,21 +119,54 @@ def get_moons_by_kingdom():
     return moons_by_kingdom
 
 
-def kingdom_bw_better(k_img):
+# checks if moon text is possibly present
+def is_moon(img):
+    img = img.resize((400, 40))
+    new_im_arr = np.ones((50, 650, 3)).astype(np.uint8) * 255
+    new_im_arr[5:-5, 125:-125] = np.array(img)
+    new_im = Image.fromarray(new_im_arr)
+    return is_text(new_im, cert=0.5)
+
+
+# Checks to see if image is talkatoo text or random pixels
+def is_text(img, cert=TEXT_CERTAINTY):
+    tc_tensor = transform(img).unsqueeze(dim=0).type(torch.float32)
+    probs = torch.softmax(text_classifier(tc_tensor), dim=1)
+    return probs[0][0] >= cert  # 0 means it's text, 1 means it's not. So this returns False for no text/uncertain
+
+
+# Make black and white image of purple coin counter
+def kingdom_bw_better(k_img, white=240):
     image_arr = np.array(k_img)
-    red = image_arr[:, :, 0] <= 240
-    green = image_arr[:, :, 1] <= 240
-    blue = image_arr[:, :, 2] <= 240
+    red = image_arr[:, :, 0] <= white
+    green = image_arr[:, :, 1] <= white
+    blue = image_arr[:, :, 2] <= white
     image_arr[:, :, 0] = np.maximum.reduce([red, green, blue]) * 255  # if any untrue make white, else black
     image_arr[:, :, 1] = image_arr[:, :, 2] = image_arr[:, :, 0]
     return Image.fromarray(image_arr.astype(np.uint8))
 
 
-# Checks to see if image is talkatoo text or random pixels
-def not_actually_text(img):
-    tc_tensor = transform(img).unsqueeze(dim=0).type(torch.float32)
-    probs = torch.softmax(text_classifier(tc_tensor), dim=1)
-    return probs[0][0] < TEXT_CERTAINTY  # 0 means it's text, 1 means it's not. So this returns False for no text/uncertain
+def match_moon_text(moon_img, mode="Unlocked"):
+    ocr_text = pytesseract.image_to_string(moon_img, lang=TRANSLATE_FROM_TESS, config='--psm 6')
+    ocr_text = correct(ocr_text.strip())
+    if len(ocr_text) < 3:
+        return None
+
+    max_corr, ans, possible = check_matches(ocr_text)
+    print(possible)
+    if max_corr >= SCORE_THRESHOLD:  # If any reasonable matches
+        best_matches = len(ans)
+        if best_matches == 1:
+            print(mode, "a moon! Best Match:\n\t{} (score={})\n".format(ans[0]["english"], max_corr))
+        else:
+            print(mode, "multiple moons! Best Matches({} total): {} (score={})".format(best_matches, " OR ".join([poss["english"] for poss in ans]), max_corr))
+        return ans
+    return None
+
+
+# Make moon black and white image
+def moon_bw(img):
+    return kingdom_bw_better(img, white=230)
 
 
 # Replacement Levenshtein distance cost function
@@ -199,7 +232,7 @@ def update_kingdom(img):
     result = int(torch.argmax(probs))
     if result != 13 and probs[0][result] > KINGDOM_CERTAINTY:  # 13 is "Other" in my model
         return kingdom_list[result], CHECK_KINGDOM_EVERY
-    return None, 5  # Was not able to determine kingdom, check in 5 iterations hoping that something's changed
+    return None, 10  # Was not able to determine kingdom, check in 10 iterations hoping that something's changed
 
 
 # Read moon translation data
@@ -221,9 +254,9 @@ for moon in moonlist:
 
 kingdom_list = ("Cap", "Cascade", "Sand", "Lake", "Wooded", "Lost", "Metro", "Seaside",
                 "Snow", "Luncheon", "Bowsers", "Moon", "Mushroom")  # to store class values, DO NOT CHANGE ORDER
-# active_kingdoms = ["Cascade", "Sand", "Wooded", "Lake", "Lost", "Metro", "Snow", "Seaside", "Luncheon", "Bowsers"]
 current_kingdom = kingdom_list[0]  # Start in first kingdom (fine to start in Cap)
 mentioned_moons = []  # list of moons mentioned by Talkatoo
+collected_moons = []  # list of auto-recognized collected moons
 
 # Load kingdom and text recognizers
 kindom_classifier = torch.jit.load("KingdomModel.zip")  # Pretrained kingdom recognizer, output 0-13 inclusive
@@ -243,10 +276,12 @@ elif this_OS == "Linux" or this_OS == "Darwin":  # Darwin signifies Mac
 # Final setup variables
 change_kingdom = ""  # Confirmation variable for kingdom changes
 check_kingdom_in = 5  # Check right after start
-old_time = time.time()
+kingdom_borders = (161, 27, 211, 77)
+moon_borders = (400, 525, 900, 575)
+talkatoo_borders = (350, 565, 1000, 615)
 text_potential = 0  # So we don't read partial text
-x1, y1, x2, y2 = 350, 565, 1000, 615  # For talkatoo text
-x3, y3, x4, y4 = 163, 27, 213, 77  # For kingdom image
+old_time = time.time()
+last_moon = old_time
 stream = cv2.VideoCapture(VIDEO_INDEX)  # Set up capture card
 borders = determine_borders(Image.fromarray(cv2.cvtColor(stream.read()[1], cv2.COLOR_BGR2RGB)))  # Find borders to crop every iteration
 eel.init('gui')  # Initialize the gui package
@@ -264,12 +299,12 @@ while True:
     grabbed, frame = stream.read()
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # Resizing image needed for distortion correction. NEAREST is ~8x faster than default but riskier so still testing
-    image = Image.fromarray(image[borders[1]:borders[3], borders[0]:borders[2]]).resize((IM_WIDTH, IM_HEIGHT), resample=Image.NEAREST)
+    image = Image.fromarray(image[borders[1]:borders[3], borders[0]:borders[2]]).resize((IM_WIDTH, IM_HEIGHT), resample=Image.BICUBIC)
 
     # Check kingdom on a loop counter, every CHECK_KINGDOM_EVERY iterations
     check_kingdom_in -= 1
     if check_kingdom_in == 0:
-        kingdom_checker = kingdom_bw_better(image.crop((x3, y3, x4, y4)))  # Must be 50x50 to work in model
+        kingdom_checker = kingdom_bw_better(image.crop(kingdom_borders))  # Must be 50x50 to work in model
         new_kingdom, check_kingdom_in = update_kingdom(kingdom_checker)
         if new_kingdom and new_kingdom != current_kingdom:  # strong match for new
             if change_kingdom == new_kingdom:  # make sure we get two in a row of the same kingdom
@@ -279,34 +314,28 @@ while True:
             else:
                 change_kingdom = new_kingdom
 
-    # Make yellow pixels black, all else white (eliminated prechecking since this is so fast now)
-    talkatoo_text, poss_text = talkatoo_preprocess_better(image.crop((x1, y1, x2, y2)), current_kingdom)
+    # Moon recognition
+    if new_time - last_moon > 5:
+        moon_checker = moon_bw(image.crop(moon_borders))
+        if is_moon(moon_checker):
+            moon_matches = match_moon_text(moon_checker, mode="Collected")
+            if moon_matches:
+                collected_moons.append(moon_matches)
+                text_potential = 0
+                last_moon = new_time
+                continue
+
+    # Talkatoo text recognition
+    talkatoo_text, poss_text = talkatoo_preprocess_better(image.crop(talkatoo_borders), current_kingdom)
     if poss_text:
         text_potential += 1
-        if text_potential * frame_time <= 0.19 or text_potential < 2 or frame_time > 0.3:  # tuned values
-            continue
+        if text_potential * frame_time > 0.19 and text_potential >= 2 and frame_time <= 0.3:  # Not waiting on a match
+            text_potential = 0
+            if is_text(talkatoo_text):  # Use text classifier to hopefully avoid unnecessary OCR passes
+                moon_matches = match_moon_text(talkatoo_text, mode="Unlocked")  # Recognize, clean, and match the output string
+                if moon_matches:  # Found at least one match
+                    if not mentioned_moons or moon_matches != mentioned_moons[-1]:  # Allow nonconsecutive duplicates
+                        mentioned_moons.append(moon_matches)
     else:
         text_potential = 0
-        continue
-
-    text_potential = 0
-    # Use Text Classifier to hopefully avoid unnecessary OCR passes
-    if not_actually_text(talkatoo_text):
-        continue
-
-    # Recognize and clean the output string
-    data = pytesseract.image_to_string(talkatoo_text, lang=TRANSLATE_FROM_TESS, config='--psm 6')
-    data = correct(data.strip())
-    if len(data) < 3:
-        continue
-
-    max_corr, ans, possible = check_matches(data)
-    print(possible)
-    if max_corr >= SCORE_THRESHOLD:  # If any reasonable matches
-        best_matches = len(ans)
-        if best_matches == 1:
-            print("Best Match:\n\t{} (score={})\n".format(ans[0]["english"], max_corr))
-        else:
-            print("Best Matches({} total): {} (score={})".format(best_matches, " OR ".join([poss["english"] for poss in ans]), max_corr))
-        if not mentioned_moons or ans != mentioned_moons[-1]:
-            mentioned_moons.append(ans)
+        
