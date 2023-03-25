14c555351e2cc7f1093553f1ed7ac5436634c90c
AzHarcos <luc718@hotmail.de>
2023-03-25 16:42:01 +0100

    use traditional chinese as default language, initialize all boolean settings as false

--- vue/src/stores/settings.js
+++ vue/src/stores/settings.js
@@ -3,11 +3,11 @@ import { defineStore } from 'pinia';
 export const useSettings = defineStore('settings', {
   state: () => {
     return {
-      inputLanguage: 'chinese_simplified',
+      inputLanguage: 'chinese_traditional',
       outputLanguage: 'english',
       includePostGame: false,
       includeWithoutTalkatoo: false,
-      woodedFirst: true,
+      woodedFirst: false,
       seasideFirst: false,
       isHardcore: false,
       skipResetConfirmation: false,
