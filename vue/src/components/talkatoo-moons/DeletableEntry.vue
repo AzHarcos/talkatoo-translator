93cdd03f332421034634fa3050769836bf9d6464
AzHarcos <luc718@hotmail.de>
2023-08-03 23:06:01 +0200

    add moon images

--- vue/src/components/talkatoo-moons/DeletableEntry.vue
+++ vue/src/components/talkatoo-moons/DeletableEntry.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { useState } from '../../stores/state';
+  import { ref } from 'vue';
 
   const props = defineProps({
     moon: Object,
@@ -7,6 +8,9 @@
   });
 
   const state = useState();
+
+  const showImage = ref(false);
+  const showOverlay = ref(false);
 
   function deleteMoon() {
     state.deleteMoon(props.moon, props.isCollected);
@@ -20,6 +24,11 @@
         <div v-bind="props" class="d-flex flex-grow-1">
           <slot></slot>
           <v-icon
+            @click="showImage = !showImage"
+            :icon="showImage ? 'mdi-eye-off' : 'mdi-eye'"
+            class="clickable ml-3"
+            size="20"></v-icon>
+          <v-icon
             v-if="isHovering"
             @click="deleteMoon"
             icon="mdi-delete"
@@ -28,6 +37,23 @@
         </div>
         <slot name="tooltip"></slot>
       </div>
+      <img
+        v-show="showImage"
+        class="ma-4"
+        :src="`http://localhost:8083/assets/moons/${moon.kingdom}/${moon.id}.png`"
+        rel="preload"
+        @click="showOverlay = true" />
+      <v-overlay v-model="showOverlay" class="justify-center align-center" width="60%">
+        <v-img
+          :src="`http://localhost:8083/assets/moons/${moon.kingdom}/${moon.id}.png`"
+          cover></v-img>
+      </v-overlay>
     </template>
   </v-hover>
 </template>
+
+<style scoped>
+  img {
+    width: 350px;
+  }
+</style>
